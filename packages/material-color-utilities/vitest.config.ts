3cb4261a5db981a086d4b2145f961ca49da5c6e0
deminearchiver <minearchiver@gmail.com>
2025-10-02 11:32:42 +0300

    Removed WASM package, upgraded Yarn and NPM dependencies

--- packages/material-color-utilities/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-import wasm from "vite-plugin-wasm";
-
-export default defineConfig({
-  build: {
-    target: "esnext",
-  },
-  plugins: [wasm()],
-});
